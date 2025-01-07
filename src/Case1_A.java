3c79106b723d09f1dbdec811f5051ea047b168d2
VikramDesai <vikramdesai4183@gmail.com>
2025-01-07 22:09:17 +0530

    some important cases while using 'throws' keyword

---
+++ src/Case1_A.java
@@ -0,0 +1,17 @@
+//note 1: 'throws' keyword only applicable for methods and constructor, not for classes
+
+//		  here we will check for classes first
+
+public class Case1_A  throws Exception{
+
+	public static void main(String[] args) {
+		System.out.println("'throws' keyword not applicable for classes");
+
+	}
+
+}
+
+/*
+	output: Unresolved compilation problem
+
+*/
---
+++ src/Case1_B.java
@@ -0,0 +1,16 @@
+//note 1: 'throws' keyword only applicable for methods and constructor, not for classes
+
+public class Case1_B {
+	Case1_B() throws Exception {
+		
+	}
+	public void method1() throws Exception{
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("'throws' keyword only applicable for methods and constructor");
+		
+
+	}
+
+}
