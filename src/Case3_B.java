d746eb511c071acb8c7e0ef64d2fbce60b8bebcb
VikramDesai <vikramdesai4183@gmail.com>
2025-01-07 17:09:45 +0530

    Some important cases about 'throw' keyword in java

--- src/Case3_B.java
+++ src/Case3_B.java
@@ -1,6 +1,7 @@
 //note 1: 'throw' keyword is only applicable for 'Throwable' (Exceptions and error) types
 //			But here we will see how we can resolve this problem if we want to use throw keyword for Non-Throwable types
 
+//for that we need to extends Non-Throwable class
 public class Case3_B extends RuntimeException{
 
 	public static void main(String[] args) {
@@ -9,3 +10,10 @@ public class Case3_B extends RuntimeException{
 	}
 
 }
+
+/*
+	output - RE:Exception in thread "main" Case3_B
+
+*/
+
+//Here we have got Runtime error
